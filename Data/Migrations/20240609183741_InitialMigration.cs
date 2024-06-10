4c123c998a689b93dbabe3c13acf069b502d551e
Ruan <ruan.oliveiralopes@hotmail.com>
2024-06-09 22:09:47 -0300

    Database Migrations

---
+++ Data/Migrations/20240609183741_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BasicAuthenticationAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EndUsers",
+                columns: table => new
+                {
+                    EndUserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EndUsers", x => x.EndUserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EndUsers");
+        }
+    }
+}
---
+++ Data/Migrations/20240609212638_admin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasicAuthenticationAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class admin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "EndUsers",
+                columns: new[] { "EndUserId", "FirstName", "LastName", "Password", "Username" },
+                values: new object[] { 1, "Jerry", "Yrrej", "$2a$11$822agt0y59tvOuQZK9URVOB37XSUXwyJ.7lzOpMJuECEb.OJKP3Wa", "admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EndUsers",
+                keyColumn: "EndUserId",
+                keyValue: 1);
+        }
+    }
+}
